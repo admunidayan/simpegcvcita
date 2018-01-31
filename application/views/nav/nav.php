013aac7d6adfda3ef1d81d3117208ad6fe760870
admunidayan <35984858+admunidayan@users.noreply.github.com>
2018-01-31 12:21:13 +0700

    config awal

---
+++ application/views/nav/nav.php
@@ -0,0 +1,13 @@
+<div class="navbox">
+	<h6 class="text-secondary">Navigator</h6>
+	<ul class="navbar-nav mr-auto">
+		<a href="<?php echo base_url('index.php/admin/dashboard/') ?>"><li class="linkmenu"><i class="fa fa-desktop text-info"></i> Beranda</li></a>
+		<a href="<?php echo base_url('index.php/admin/profil/') ?>"><li class="linkmenu"><i class="fa fa-user-o text-info"></i> Profil</li></a>
+		<a href="<?php echo base_url('index.php/admin/jadwal/') ?>"><li class="linkmenu"><i class="fa fa-calendar-o text-info"></i> Jadwal Kuliah</li></a>
+		<a href="<?php echo base_url('index.php/admin/nilai/') ?>"><li class="linkmenu"><i class="fa fa-list text-info"></i> Lihat Semua Nilai</li></a>
+		<a href="<?php echo base_url('index.php/admin/nilai/semester') ?>"><li class="linkmenu"><i class="fa fa-tag text-info"></i> Nilai Persemester</li></a>
+		<a href="<?php echo base_url('index.php/admin/aktivitas/') ?>"><li class="linkmenu"><i class="fa fa-folder-o text-info"></i> Aktifitas Kuliah</li></a>
+		<li class="linkmenu"><i class="fa fa-shopping-cart text-info"></i> Pembayaran</li>
+		<li class="linkmenu"><i class="fa fa-trophy text-info"></i> Prestasi</li>
+	</ul>
+</div>
