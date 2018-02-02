6723ba079ddcfbb1d3f8a1d13e24b1cbaa8dde1c
admunidayan <35984858+admunidayan@users.noreply.github.com>
2018-02-02 14:08:44 +0700

    ubah cript tampilan

---
+++ application/views/admin/beranda-v.php
@@ -0,0 +1,3 @@
+<div style="margin-top: 14px; padding: 30px">
+	Nanti Grafik disini
+</div>
--- application/views/nav/nav.php
+++ application/views/nav/nav.php
@@ -2,5 +2,18 @@
 	<h6 class="text-secondary">Navigator</h6>
 	<ul class="navbar-nav mr-auto">
 		<a href="<?php echo base_url('index.php/admin/dashboard/') ?>"><li class="linkmenu"><i class="fa fa-desktop text-info"></i> Beranda</li></a>
+		<a href="<?php echo base_url('index.php/admin/dashboard/') ?>"><li class="linkmenu"><i class="fa fa-users text-info"></i> Pegawai <span class="badge badge-secondary">1</span></li></a>
+		<a href="<?php echo base_url('index.php/admin/dashboard/') ?>"><li class="linkmenu"><i class="fa fa-university text-info"></i> Unit Kerja <span class="badge badge-secondary">1</span></li></a>
+		<a href="<?php echo base_url('index.php/admin/dashboard/') ?>"><li class="linkmenu"><i class="fa fa-check-circle text-info"></i> Satuan Kerja <span class="badge badge-secondary">1</span></li></a>
+		<a href="<?php echo base_url('index.php/admin/dashboard/') ?>"><li class="linkmenu"><i class="fa fa-eye text-info"></i> PKK <span class="badge badge-secondary">1</span></li></a>
+		<a href="<?php echo base_url('index.php/admin/dashboard/') ?>"><li class="linkmenu"><i class="fa fa-desktop text-info"></i> Golongan <span class="badge badge-secondary">1</span></li></a>
+		<a href="<?php echo base_url('index.php/admin/dashboard/') ?>"><li class="linkmenu"><i class="fa fa-desktop text-info"></i> Eselon <span class="badge badge-secondary">1</span></li></a>
+		<a href="<?php echo base_url('index.php/admin/dashboard/') ?>"><li class="linkmenu"><i class="fa fa-desktop text-info"></i> Pelatihan <span class="badge badge-secondary">1</span></li></a>
+		<a href="<?php echo base_url('index.php/admin/dashboard/') ?>"><li class="linkmenu"><i class="fa fa-desktop text-info"></i> Jabatan <span class="badge badge-secondary">1</span></li></a>
+		<a href="<?php echo base_url('index.php/admin/dashboard/') ?>"><li class="linkmenu"><i class="fa fa-desktop text-info"></i> Status Jabatan <span class="badge badge-secondary">1</span></li></a>
+		<a href="<?php echo base_url('index.php/admin/dashboard/') ?>"><li class="linkmenu"><i class="fa fa-desktop text-info"></i> Penghargaan <span class="badge badge-secondary">1</span></li></a>
+		<a href="<?php echo base_url('index.php/admin/dashboard/') ?>"><li class="linkmenu"><i class="fa fa-desktop text-info"></i> Hukuman <span class="badge badge-secondary">1</span></li></a>
+		<a href="<?php echo base_url('index.php/admin/dashboard/') ?>"><li class="linkmenu"><i class="fa fa-desktop text-info"></i> Lokasi Pelatihan <span class="badge badge-secondary">1</span></li></a>
+		<a href="<?php echo base_url('index.php/admin/dashboard/') ?>"><li class="linkmenu"><i class="fa fa-desktop text-info"></i> Lokasi Kerja <span class="badge badge-secondary">1</span></li></a>
 	</ul>
 </div>
